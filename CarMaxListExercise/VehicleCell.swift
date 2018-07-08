404b5de8c4f3a43c1a3d721a491e2ff29474c0ef
jonathanthornburg <reliable2192@gmail.com>
2018-07-08 13:55:59 -0400

    initial commit

---
+++ CarMaxListExercise/VehicleCell.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleCell.swift
+//  CarMaxListExercise
+//
+//  Created by jonathan thornburg on 7/8/18.
+//  Copyright © 2018 jonathan thornburg. All rights reserved.
+//
+
+import UIKit
+
+class VehicleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
